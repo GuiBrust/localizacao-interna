0c04a1f1d3578e65836926e6eb49437f2036738f
Guilherme Brust <gbrust15@gmail.com>
2023-11-09 01:57:25 -0300

    ajuste componente header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,44 +1,104 @@
-import { useContext } from 'react';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  HStack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import Image from "next/image";
+import { FiLogOut } from "react-icons/fi";
+import { useContext } from "react";
+import Link from "next/link";
 import styles from './styles.module.scss';
-import Link from 'next/link';
-import Image from 'next/image';
-import { FiLogOut } from 'react-icons/fi';
 
-import { AuthContext } from '../../contexts/AuthContext';
+import { AuthContext } from "../../contexts/AuthContext";
 
-export function Header() {
+interface Props {
+  children: React.ReactNode;
+}
 
-  const { signOut } = useContext(AuthContext);
+const NavLink = (props: Props) => {
+  const { children, href } = props;
 
   return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <a href="/dashboard" >
-          <Image src="/header.png" alt="Dashboard" width={230} height={50} />
-        </a>
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={href}
+    >
+      {children}
+    </Box>
+  );
+};
 
-        <nav className={styles.menuNav}>
-          <Link href="/usuario" passHref>
-            <a>Usuário</a>
-          </Link>
+export function Header() {
+  const { signOut } = useContext(AuthContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-          <Link href="/bloco" passHref>
-            <a>Bloco</a>
-          </Link>
-
-          <Link href="/andar" passHref>
-            <a>Andar</a>
-          </Link>
-
-          <Link href="/planta_baixa" passHref>
-            <a>Planta Baixa</a>
-          </Link>
-
-          <button onClick={signOut}>
-            <FiLogOut color='#000' size={20} />
+  return (
+    <>
+      <Box px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Link href="/dashboard" passHref>
+              <a>
+                <Image
+                  src="/header.png"
+                  alt="Dashboard"
+                  width={230}
+                  height={50}
+                />
+              </a>
+            </Link>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              <NavLink href="/usuario">Usuário</NavLink>
+              <NavLink href="/bloco">Bloco</NavLink>
+              <NavLink href="/andar">Andar</NavLink>
+              <NavLink href="/planta_baixa">Planta Baixa</NavLink>
+            </HStack>
+          </HStack>
+          <button className={styles.botaoSair} onClick={signOut}>
+            <FiLogOut color="#000" size={23} />
           </button>
-        </nav>
-      </div>
-    </header>
-  )
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              <NavLink href="/usuario">Usuário</NavLink>
+              <NavLink href="/bloco">Bloco</NavLink>
+              <NavLink href="/andar">Andar</NavLink>
+              <NavLink href="/planta_baixa">Planta Baixa</NavLink>
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
 }
